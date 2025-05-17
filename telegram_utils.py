c187c0bab93d2dc8c438e794abeb48a0e27c13f4
PhillipeMarq <phillipemarques22@gmail.com>
2025-05-17 20:51:56 -0300

    Update telegram_utils.py

--- telegram_utils.py
+++ telegram_utils.py
@@ -3,7 +3,7 @@ import os
 import telegram
 
 def send_message(text):
-    bot = telegram.Bot(token=os.getenv("BOT_TOKEN"))
-    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+    bot = telegram.Bot(token=os.getenv("8124502590:AAHOzEYywnp6sNuEyDn9Lz4ZNyMIIfF8RiM"))
+    chat_id = os.getenv("7178592047")
     if chat_id:
         bot.send_message(chat_id=chat_id, text=text)
